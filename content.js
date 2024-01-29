4bb35e62312c9f3db4666fae204ebf1584df04b0
ThatLukeDev <luke@l2j.co.uk>
2024-01-29 07:56:13 +0000

    google image overlay onto everything - GOOGLE GOOGLE

--- content.js
+++ content.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+let srcUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/800px-Google_2015_logo.svg.png";
+
+let images = Array.from(document.getElementsByTagName("img"));
+
+images.forEach((image) => {
+    let bounds = image.getBoundingClientRect();
+
+    let overlay = document.createElement("img");
+
+    overlay.src = srcUrl;
+
+    overlay.width = bounds.right - bounds.left;
+    overlay.height = bounds.bottom - bounds.top;
+
+    overlay.style.zIndex = 32767;
+    overlay.style.position = "fixed";
+
+    overlay.style.top = bounds.top + "px";
+    overlay.style.left = bounds.left + "px";
+
+    image.parentNode.insertBefore(overlay, image.nextSibling);
+})
